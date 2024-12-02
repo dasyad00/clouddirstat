2eea8570c7a2f7ae67c368ff4d78b8e9faab356f
Danang Rahmatullah <dasyad00@gmail.com>
2024-12-02 22:07:21 +0100

    Run Dropbox folder getter recursively

--- src/utils/dropbox/service.ts
+++ src/utils/dropbox/service.ts
@@ -1,46 +1,55 @@
 import { Dropbox, files as dbxFiles } from "dropbox";
 import { CloudFile, CloudFolder, CloudItem } from "../../types/cloudDrive";
+import { getCloudFolderSize } from "../cloudFolder";
 
 // Get the list of files and folders
-export async function getFiles(dbx: Dropbox, path: string = ""): Promise<CloudItem[]> {
+export async function getFiles(
+  dbx: Dropbox,
+  path: string = ""
+): Promise<CloudItem[]> {
   try {
     let response = await dbx.filesListFolder({ path });
     let hasMore = response.result.has_more;
     let cursor = response.result.cursor;
     const output = response.result.entries;
     while (hasMore) {
-        response = await dbx.filesListFolderContinue({ cursor });
-        response.result.entries;
-        hasMore = response.result.has_more;
-        cursor = response.result.cursor
-        const moreOutput = response.result.entries;
-        output.concat(moreOutput);
+      response = await dbx.filesListFolderContinue({ cursor });
+      response.result.entries;
+      hasMore = response.result.has_more;
+      cursor = response.result.cursor;
+      const moreOutput = response.result.entries;
+      output.concat(moreOutput);
     }
 
-    const folders: dbxFiles.FolderMetadataReference[] = output.filter((metadata) => metadata[".tag"] === "folder");
-    const files: dbxFiles.FileMetadataReference[] = output.filter((metadata) => metadata[".tag"] === "file");
+    const folders: dbxFiles.FolderMetadataReference[] = output.filter(
+      (metadata) => metadata[".tag"] === "folder"
+    );
+    const files: dbxFiles.FileMetadataReference[] = output.filter(
+      (metadata) => metadata[".tag"] === "file"
+    );
 
-    const cloudFolders: CloudFolder[] = folders.map((folder) => {
-        // TODO
-        // const items = await getFiles(dbx, folder.path_lower);
+    const cloudFoldersPromise: Promise<CloudFolder>[] = folders.map(
+      async (folder) => {
+        const items = await getFiles(dbx, folder.id);
+        const folderSize = getCloudFolderSize(items);
         return {
-            id: folder.id,
-            name: folder.name,
-            iconLink: folder.preview_url ?? "",
-            size: 0,
-            children: [],
+          id: folder.id,
+          name: folder.name,
+          iconLink: folder.preview_url ?? "",
+          size: folderSize,
+          children: items,
         };
-    });
-
+      }
+    );
     const cloudFiles: CloudFile[] = files.map((file) => {
-        return {
-            id: file.id,
-            name: file.name,
-            size: file.size,
-            iconLink: file.preview_url ?? "",
-        };
+      return {
+        id: file.id,
+        name: file.name,
+        size: file.size,
+        iconLink: file.preview_url ?? "",
+      };
     });
-
+    const cloudFolders = await Promise.all(cloudFoldersPromise);
     return (cloudFolders as CloudItem[]).concat(cloudFiles);
   } catch (error) {
     console.error("Error fetching files from Dropbox:", error);
