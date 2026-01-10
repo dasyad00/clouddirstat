2c36b6d1a389b15af981892a1bdc18e980029c47
dasyad00 <dasyad00@gmail.com>
2026-01-10 13:00:04 +0000

    use tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import App from "./App.tsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthContextProvider } from "./contexts/AuthContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_DRIVE_CLIENT_ID}>
     <React.StrictMode>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </QueryClientProvider>
     </React.StrictMode>
   </GoogleOAuthProvider>
 );
--- src/pages/FileManagerPage.tsx
+++ src/pages/FileManagerPage.tsx
@@ -7,7 +7,6 @@ import {
   Link,
   Typography,
 } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
 import {
   CloudFolder,
   CloudItem,
@@ -17,36 +16,24 @@ import { ListView } from "../components/ListView";
 import RefreshIcon from "@mui/icons-material/Refresh";
 import { useAuthContext } from "../contexts/AuthContext";
 import Layout from "../components/Layout";
+import { useQuery } from "@tanstack/react-query";
 
 const FileExplorerPage: React.FC = () => {
   const { authState } = useAuthContext();
   const navigate = useNavigate();
   const params = useParams();
 
-  const [rootFolder, setRootFolder] = useState<CloudFolder>();
-  const [folderPath, setFolderPath] = useState<CloudFolder[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const fetchData = useCallback(
-    async (path?: string) => {
-      if (authState === null) return;
-      try {
-        const currentFolder = await authState.getFolder(path);
-        const folderPath = await authState.getFolderPath(currentFolder.id);
-        setRootFolder(currentFolder);
-        setFolderPath(folderPath);
-      } catch (error) {
-        console.error("Error fetching files:", error);
-      }
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ["folder", authState, params["*"]],
+    queryFn: async () => {
+      if (!authState) return;
+      const [rootFolder, folderPath] = await Promise.all([
+        authState.getFolder(params["*"]),
+        authState.getFolderPath(params["*"] || "root"),
+      ]);
+      return { rootFolder, folderPath };
     },
-    [authState],
-  );
-
-  useEffect(() => {
-    if (authState === null || authState.token === "") return;
-    setIsLoading(true);
-    fetchData(params["*"]).finally(() => setIsLoading(false));
-  }, [fetchData, authState, params]);
+  });
 
   function onBreadcrumbClick(
     _: React.MouseEvent<unknown>,
@@ -65,8 +52,8 @@ const FileExplorerPage: React.FC = () => {
   }
 
   function onNavigateUp() {
-    if (folderPath.length > 1) {
-      const parentFolder = folderPath[folderPath.length - 2];
+    if (data?.folderPath && data.folderPath.length > 1) {
+      const parentFolder = data.folderPath[data.folderPath.length - 2];
       navigate(`/explorer/${parentFolder.id}`);
     }
   }
@@ -89,12 +76,12 @@ const FileExplorerPage: React.FC = () => {
         sx={{ m: 2 }}
         variant="contained"
         startIcon={<RefreshIcon />}
-        onClick={() => fetchData(params["*"])}
+        onClick={() => refetch()}
       >
         Reload
       </Button>
       <Breadcrumbs aria-label="breadcrumbs">
-        {folderPath.map((folder) => (
+        {data?.folderPath?.map((folder) => (
           <Link
             key={folder.id}
             onClick={(event) => onBreadcrumbClick(event, folder)}
@@ -103,11 +90,11 @@ const FileExplorerPage: React.FC = () => {
           </Link>
         ))}
       </Breadcrumbs>
-      {folderPath.length > 1 && <Button onClick={onNavigateUp}>Up</Button>}
-      {rootFolder && (
+      {data?.folderPath && data.folderPath.length > 1 && <Button onClick={onNavigateUp}>Up</Button>}
+      {data?.rootFolder && (
         <ListView
-          files={rootFolder.children || []}
-          onNavigateUp={() => folderPath.length > 1 && onNavigateUp()}
+          files={data.rootFolder.children || []}
+          onNavigateUp={() => data.folderPath.length > 1 && onNavigateUp()}
           onItemDoubleClick={onItemDoubleClick}
         />
       )}
