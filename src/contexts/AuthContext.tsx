a7f60ef81b7bec07bd81092114dc89dc172269c5
dasyad00 <dasyad00@gmail.com>
2026-01-13 20:11:45 +0000

    store authstate in localStorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,15 @@
-import { ReactNode, createContext, useContext, useState } from "react";
-import { CloudAuthState } from "../types/cloudProvider";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { CloudAuthState, CloudProvider } from "../types/cloudProvider";
+import { GoogleAuthState } from "../utils/google/provider";
+import { DropboxAuthState } from "../utils/dropbox/provider";
+
+const authStateKey = "authState";
 
 interface AuthContextProviderProps {
   children: ReactNode;
@@ -13,10 +23,35 @@ interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
+function getInitialAuthState(): CloudAuthState | null {
+  const json = localStorage.getItem(authStateKey);
+  if (!json) {
+    return null;
+  }
+
+  const storedState = JSON.parse(json) as { provider: CloudProvider; token: string };
+  switch (storedState.provider.id) {
+    case "google":
+      return new GoogleAuthState(storedState.token);
+    case "dropbox":
+      return new DropboxAuthState(storedState.token);
+    default:
+      return null;
+  }
+}
+
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   const [authState, setInternalAuthState] = useState<CloudAuthState | null>(
-    null,
+    getInitialAuthState,
   );
+
+  useEffect(() => {
+    if (authState) {
+      localStorage.setItem(authStateKey, JSON.stringify(authState));
+    } else {
+      localStorage.removeItem(authStateKey);
+    }
+  }, [authState]);
 
   function setAuthState(state: CloudAuthState) {
     setInternalAuthState(state);
